bbd303eb3590efc0c7c5a8c99d596992324bd62b
Abdullah Khabbab Tasib <akt629201@gmail.com>
2026-08-12 11:00:02 +0600

    Use environment secret key

--- backend/lms_backend/lms_backend/settings.py
+++ backend/lms_backend/lms_backend/settings.py
@@ -24,8 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l#cyh@&r_12icbh4(a#_d5=k^-+d*kx(ab!9_2lff4m48b0^3%'
-
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY',
+    'django-insecure-development-only-key'
+)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
