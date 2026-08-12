85e1cf6b7ea626c9433b63da7ad1d0265b480dee
Abdullah Khabbab Tasib <akt629201@gmail.com>
2026-08-12 11:37:59 +0600

    Allow production frontend CORS

--- backend/lms_backend/lms_backend/settings.py
+++ backend/lms_backend/lms_backend/settings.py
@@ -174,7 +174,7 @@ SIMPLE_JWT = {
 # CORS Settings
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:8000",
+    "https://academy-frontend-2wwc.onrender.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
