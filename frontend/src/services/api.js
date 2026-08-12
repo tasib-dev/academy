b543dc2db9f54332a2f821c54ca0b6cec3cba68d
Abdullah Khabbab Tasib <akt629201@gmail.com>
2026-08-12 11:30:39 +0600

    Connect frontend to production API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://academy-backend-ukt8.onrender.com/api';
 
 const api = axios.create({
     baseURL: API_URL,
